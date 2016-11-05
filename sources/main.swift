7992f91051dcf3b5dbcb31e4f48e2e375ccdcfd5
= <stephen@sstctf.org>
2016-11-05 15:31:12 -0700

    Added security measures to prevent unauthorized users from accessing system

--- sources/main.swift
+++ sources/main.swift
@@ -8,13 +8,40 @@
 
 import Foundation;
 import Socks;
+import SwiftShell;
+
+typealias Byte=UInt8;
 
 let address = InternetAddress.localhost(port: 12345);
 
+// get GIDs for user
+let gids = run("id", "-G").toBytes();
+
 do {
+    // Try to connect to server
     let client = try TCPClient(address: address);
-    try client.send(bytes: [0]);
+    // Try to send bytes
+    try client.send(bytes: [0] + gids);
+    // Save response
+    let response = try client.receiveAll();
+    if response[0] == 6 {
+        print("User is authorized. Publishing website");
+    } else if response[0] == 21 {
+        print("User is not authorized. Publishing not permitted");
+        try client.close();
+        exit(2);
+    } else if response[0] == 0 {
+        print("Invalid command");
+        try client.close();
+        exit(3);
+    } else {
+        print("Unexpected response");
+        try client.close();
+        exit(4);
+    }
+    // Close connection
     try client.close();
 } catch {
     print("Error contacting publish daemon. Check it is running.");
+    exit(1);
 }
